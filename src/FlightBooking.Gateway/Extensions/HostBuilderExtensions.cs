f14295057030b47bffd3a75f9ae8a87c2d8ffb60
Maxim <mad-maxkozlov@mail.ru>
2022-11-06 12:25:22 +0300

    #1 init src

---
+++ src/FlightBooking.Gateway/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,14 @@
+using Serilog;
+
+namespace FlightBooking.Gateway.Extensions;
+
+internal static class HostBuilderExtensions
+{
+    internal static IHostBuilder SetupSerilog(this IHostBuilder hostBuilder)
+    {
+        return hostBuilder.UseSerilog((hostingContext, configuration) =>
+        {
+            configuration.ReadFrom.Configuration(hostingContext.Configuration);
+        });
+    }
+}
