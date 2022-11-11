57641370411e54bbd51aec8e78074b00b0aa0d09
Maxim <mad-maxkozlov@mail.ru>
2022-11-11 19:02:24 +0300

    #1 try fix

--- scripts/wait-script.sh
+++ scripts/wait-script.sh
@@ -2,6 +2,10 @@
 
 IFS="," read -ra PORTS <<<"$WAIT_PORTS"
 
+echo "pwd: $(pwd)\n"
+echo "$path"
+ls
+
 PIDs=()
 for port in "${PORTS[@]}"; do
   "$(pwd)"/scripts/wait-for.sh -t 120 "localhost:$port" -- echo "Host localhost:$port is active" &
